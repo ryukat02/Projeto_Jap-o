eef61393c65289ff8b3b49666b9ba49eabceb3d8
ryukat02 <raquel.taniwaki@sptech.school>
2026-05-26 16:54:44 -0300

    atualizei o a validação de senha do cadastro, removi botões de próximo e tentar novamente do quiz, arrumei o css do quiz e o direcionamento de rota do indexJP

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get("/", function (req, res) {
     res.render("index");
 });
-router.get("/", function (req, res) {
+router.get("/jp", function (req, res) {
     res.render("indexJP");
 });
 
